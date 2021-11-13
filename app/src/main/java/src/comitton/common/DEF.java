0fb1ce72d52e691ac4babeeabc0ac4bd9943423f
mry <mryp.dev@gmail.com>
2021-11-14 00:47:29 +0900

    Develop/b (#1)
    * Java11ビルド時にファイうリスト画面でファイル数が多いときときソート有で例外発生する問題を暫定対応
    
    * Comparison method violates its general contract! 対策で漢数字等を考慮してのソート処理を行わないように変更
    
    * APK追加
    
    * Update README.md
    
    * リリース名をBに変更
    
    * APK追加

--- app/src/main/java/src/comitton/common/DEF.java
+++ app/src/main/java/src/comitton/common/DEF.java
@@ -1623,6 +1623,8 @@ public class DEF {
 			return CHTYPE_NUM;
 		}
 
+		//Comparison method violates its general contract! 対策で常に文字列として扱う
+		/*
 		switch (ch) {
 			case '上':
 			case '中':
@@ -1668,8 +1670,11 @@ public class DEF {
 			case '阡':
 			case '万':
 			case '萬':
+			case '億':
+			case '兆':
 				return CHTYPE_JNUM;
 		}
+		*/
 		return CHTYPE_CHAR;
 	}
 
